a24f0c1b50dd3bcbbf4e060a1b8ebb954c133b8d
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-01-30 18:29:56 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Anywhere Fitness</h1>
+        <nav>
+          <a>Home</a>
+          <a></a>
+          <a></a>
+          <a></a>
+          <a></a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
